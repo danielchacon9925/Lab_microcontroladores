c3dd2b8051c31f28184f286fa1f530f030078e97
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2024-01-15 20:42:31 -0600

    Funciones de configuración de entradas, salidas y TIMERs listas
    La función para mostrar en display se compone de 2. Una para mostrar el valor del tiempo y otra para que sea usada en 7 segmentos

--- Lab2/src/Lavadora.c
+++ Lab2/src/Lavadora.c
@@ -43,6 +43,125 @@ volatile state_t next_state = STATE_IDE;
 // Tipo de carga
 volatile uint8_t load = 0;
 
+//Declaracion de funciones
+// Configuraciones iniciales
+void setup();
+void SET_TIMER(uint8_t diferential_time_value);
+// Configuraciones de 7seg display
+void COUNTDOWN_TIMER(int timer);
+void LED_7s_display(int valor, int display);
 
 
 
+
+void setup()
+{
+	// Inputs
+	DDRD &= ~(LOW_LOAD_BUTTON | MEDIUM_LOAD_BUTTON | START_PAUSE_BUTTON | RESET_BUTTON);
+	DDRA &= ~HIGH_LOAD_BUTTON;
+
+	// Outputs
+	DDRA |= (LED_STATE_FILL | LED_STATE_WASH);
+	DDRB |= (LED_START_PAUSE | LED_HIGH_LOAD | LED_LOW_LOAD);
+	DDRD |= (LED_STATE_RINSE | LED_STATE_SPIN | LED_MEDIUM_LOAD);
+
+	// Habilitar pull-up en botones
+	PORTD |= (LOW_LOAD_BUTTON | MEDIUM_LOAD_BUTTON | START_PAUSE_BUTTON | RESET_BUTTON);
+	PORTA |= HIGH_LOAD_BUTTON;
+
+	//Activación de interrupciones en PCMSK 
+	PCMSK |= (1<<PCINT12); // Interrupcion carga baja D1
+
+	PCMSK2 |= (1<<PCINT11); //Interrupcion nivel media D0
+
+    PCMSK1 |= (1<<PCINT10); // Interrupcion nivel alta A2
+
+    //Configuracion GIMSK para activar interupciones
+	GIMSK |= ((1<<PCIE0) | (1<<PCIE1) | (1<<PCIE2)); //Interupciones de los botones de carga
+	GIMSK |= ((1 << INT0) | (1 << INT1)); // Interrupcion de start/pause y reset
+
+}
+
+// Función para configurar TIMER 
+void SET_TIMER(uint8_t diferential_time_value)
+{
+	// Configurar el prescaler a 64
+	TCCR0B |= (1 << CS01) | (1 << CS00);
+
+	// Definir el valor de comparación
+	OCR0A = diferential_time_value; //15624 si es cada 1s para el prescaler de 64 y un reloj de 1MHz
+
+	// Configuracion del modo de operacion CTC
+	TCCR0A |= (1 << WGM01);
+
+	// Habilitar la interrupción de comparación en la salida A
+  	TIMSK |= (1 << OCIE0A);
+
+	// Se habilitan interrupciones globales
+	sei();
+}
+
+
+/// Sección de funciones de display ////
+
+// Muestra en pantalla tiempo restante 
+void COUNTDOWN_TIMER(int timer)
+{
+	while(timer > 0)
+	{
+		LED_7s_display(timer, 1);
+		_delay_ms(10000);
+		timer--;
+	}
+	LED_7s_display(0, 0);
+}
+
+// Display 7 segmentos
+void LED_7s_display(int valor, int display)
+{
+	PORTB &= 0b10000111;
+    if (display == 0)
+	{
+		if (valor == 0) PORTB |= 0b00000000;
+
+		else if (valor == 1) PORTB |= 0b01000000; 
+
+		else if (valor == 2) PORTB |= 0b00100000; 
+
+		else if (valor == 3) PORTB |= 0b01100000;
+
+		else if (valor == 4) PORTB |= 0b00010000;
+
+		else if (valor == 5) PORTB |= 0b01010000;
+
+		else if (valor == 6) PORTB |= 0b00110000;
+
+		else if (valor == 7) PORTB |= 0b01110000;
+
+		else if (valor == 8) PORTB |= 0b00001000;
+
+		else PORTB |= 0b01001000;
+	}
+	else  // display == 1
+	{
+		if (valor == 0) PORTB |= 0b00100000;
+
+		else if (valor == 1) PORTB |= 0b01000100; 
+
+		else if (valor == 2) PORTB |= 0b00100100; 
+
+		else if (valor == 3) PORTB |= 0b01100100;
+
+		else if (valor == 4) PORTB |= 0b00010100;
+
+		else if (valor == 5) PORTB |= 0b01010100;
+
+		else if (valor == 6) PORTB |= 0b00110100;
+
+		else if (valor == 7) PORTB |= 0b01110100;
+
+		else if (valor == 8) PORTB |= 0b00001100;
+
+		else PORTB |= 0b01001100;
+	}
+}
