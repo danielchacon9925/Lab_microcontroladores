df2b1b268c9090ed5eb41cb5acf6e0578bebc6d5
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2024-01-17 19:05:00 -0600

    Código de lavadora realizado
    Hace falta mejorar la función de RESET para que apague los LEDS una vez que prenden e implementar motor

--- Lab2/src/Lavadora.c
+++ Lab2/src/Lavadora.c
@@ -10,27 +10,25 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 
-
-
 //Definicion de pines
 
 // Carga
-#define HIGH_LOAD_BUTTON (1<<PD6)
-#define MEDIUM_LOAD_BUTTON (1<<PB0)
-#define LOW_LOAD_BUTTON (1<<PA0)
+#define HIGH (1<<PA0)
+#define MEDIUM (1<<PD6)
+#define LOW (1<<PB0)
 // START/PAUSE/RESET
-#define RESET_BUTTON (1<<PD3)
-#define START_PAUSE_BUTTON (1<<PD2)
-#define LED_START_PAUSE (1<<PB1)
+#define START_PAUSE (1<<PD3)
+#define RESET (1<<PD2)
+#define START_PAUSE_LED (1<<PB1)
 // LEDs de ciclo
-#define LED_STATE_FILL (1<<PB7)
-#define LED_STATE_WASH (1<<PA2)
-#define LED_STATE_RINSE (1<<PD4)
-#define LED_STATE_SPIN (1<<PD5)
+#define FILL_LED (1<<PA2)
+#define WASH_LED (1<<PD4)
+#define RINSE_LED (1<<PD5)
+#define SPIN_LED (1<<PB7)
 // LEDS load
-#define LED_HIGH_LOAD (1<<PD0)
-#define LED_MEDIUM_LOAD (1<<PD1)
-#define LED_LOW_LOAD (1<<PA1)
+#define HIGH_LED (1<<PA1)
+#define MEDIUM_LED (1<<PD1)
+#define LOW_LED (1<<PD0)
 
 
 
@@ -41,8 +39,9 @@ typedef enum {
 	STATE_WASH,
 	STATE_RINSE,
 	STATE_SPIN,
-	STATE_FINISH
+	STATE_FINAL
 } state_t;
+
 
 // Varibles globales
 // Se define IDE como estado inicial
@@ -62,13 +61,14 @@ void SET_TIMER(uint8_t diferential_time_value);
 void COUNTDOWN_TIMER(int timer);
 void LED_7s_display(int valor, int display);
 // Máquina de estados
-void fsm();
+void FSM();
 // Estados
 uint8_t FILL(uint8_t load);
 uint8_t WASH(uint8_t load);
 uint8_t RINSE(uint8_t load);
 uint8_t SPIN(uint8_t load);
 void FINAL(uint8_t load);
+
 
 int main(void)
 {
@@ -78,41 +78,140 @@ int main(void)
 	SET_TIMER(diferential_time_value);
 	// Bucle con máquina de estados
 	while(1){
-		fsm();
+		FSM();
 	}
 	return 0;
 }
+//////// INTERRUPCIONES ////////
+
+// RESET
+ISR(INT0_vect) 
+{
+	if(PIND & RESET){
+		state = STATE_IDE;
+		PORTD &= ~RINSE_LED;
+		PORTA &= ~FILL_LED;
+		PORTD &= ~WASH_LED;
+		PORTB &= ~SPIN_LED;
+	}
+	else{
+		state = STATE_IDE;
+		PORTD |= ~RINSE_LED;
+		PORTA |= ~FILL_LED;
+		PORTD |= ~WASH_LED;
+		PORTB |= ~SPIN_LED;
+		// Reinicio de TIMER
+		diferential_time_value = 0; 
+		COUNTDOWN_TIMER( diferential_time_value);
+		SET_TIMER(diferential_time_value);		
+	}
+}
+// START/PAUSE
+ISR(INT1_vect) 
+{
+	if(PIND & START_PAUSE){
+		//Cuando el timer esta activado se detiene
+		if (TIMSK & (1<<TOIE0)){
+			TIMSK &= ~(1<<TOIE0);
+			PORTB &= ~START_PAUSE_LED;
+		}
+		//Si esta detenido lo iniciamos
+		else{
+			TIMSK |= (1<<TOIE0);
+			PORTB |= START_PAUSE_LED;
+		}
+	}
+}
+// LOW 
+ISR(PCINT0_vect)
+{
+	if(PINB & LOW){
+		load = 1;
+		PORTD |= LOW_LED;
+	}
+	else {
+		PORTD &= ~LOW_LED;
+	}
+
+}
+// MEDIUM
+ISR(PCINT2_vect) 
+{
+	if(PIND & MEDIUM){
+		load = 2;
+		PORTD |= MEDIUM_LED;
+	}
+	else {
+		PORTD &= ~MEDIUM_LED;
+	}
+
+}
+//HIGH
+ISR(PCINT1_vect) 
+{
+	if(PINA & HIGH){
+		load = 3;
+		PORTA |= HIGH_LED;
+	}
+	else {
+		PORTA &= ~HIGH_LED;
+	}
+}
+
+ISR(TIMER0_COMPA_vect) {
+  // Aqui va la rutina de interrupcion
+  // Hacer LED display cada vez que se produzca la interrupción
+	static uint8_t contador = 0;
+	contador++;
+	 
+	// Se configuró el timer0 con un prescales de 64 y reloj de 1 MHz
+	// Se ocupan 15625 ciclos del timer para que pase un segundo
+	if(contador == 15625){
+		time++;
+		time_left--;
+		//LED_7s_display()
+	}
+	else if(time == diferential_time_value){
+		//LED_7s_display(0,0)
+		TCNT0 = 0;
+		TIMSK &= ~(1<<TOIE0);
+	}
+	contador = 0;
+	time = 0;
+	time_left = 0;
+}
+
 
 
 void setup()
 {
-	// Inputs
-	DDRD &= ~(HIGH_LOAD_BUTTON | RESET_BUTTON | START_PAUSE_BUTTON );
-	DDRB &= ~MEDIUM_LOAD_BUTTON;
-	DDRA &= ~LOW_LOAD_BUTTON;
-
-	// Outputs
-	DDRA |= ( LED_LOW_LOAD | LED_STATE_WASH);
-	DDRB |= (LED_START_PAUSE | LED_STATE_FILL);
-	DDRD |= (LED_STATE_RINSE | LED_STATE_SPIN | LED_MEDIUM_LOAD | LED_HIGH_LOAD);
+	//Configuras los pines como entrada
+	DDRB &= ~LOW;
+	DDRA &= ~HIGH;
+	DDRD &= ~(MEDIUM | RESET | START_PAUSE);
+	//Configuras los pines como salida
+	DDRD |= (MEDIUM_LED | LOW_LED | WASH_LED | RINSE_LED);
+	DDRB |= (START_PAUSE_LED | SPIN_LED);
+	DDRA |=	(HIGH_LED | FILL_LED); //revisar como poner como salida pin A2
 
 	// Habilitar pull-up en botones
-	PORTD |= (START_PAUSE_BUTTON | RESET_BUTTON | HIGH_LOAD_BUTTON);
-	PORTB |= MEDIUM_LOAD_BUTTON;
-	PORTA |= LOW_LOAD_BUTTON;
-
+    PORTD |= (START_PAUSE | RESET | MEDIUM);
+	PORTB |= LOW; 
+	PORTA |= HIGH;
 
 	//Configucion registro PCMSK para activar interupciones
-	PCMSK |= (1<<PCINT8); // Interrupcion carga baja A0
+	PCMSK |= (1<<PCINT0); // Interrupcion carga baja B0
 
-	PCMSK2 |= (1<<PCINT0); //Interrupcion nivel media B0
+	PCMSK1 |= (1<<PCINT8); // Interrupcion nivel alta A0
+    
+	PCMSK2 |= (1<<PCINT17); //Interrupcion nivel media D6
 
-    PCMSK1 |= (1<<PCINT17); // Interrupcion nivel alta D6
-
-    //Configuracion GIMSK para activar interupciones
+	//Configuracion GIMSK para activar interupciones
 	GIMSK |= ((1<<PCIE0) | (1<<PCIE1) | (1<<PCIE2)); //Interupciones de los botones de carga
 	GIMSK |= ((1 << INT0) | (1 << INT1)); // Interrupcion de start/pause y reset
 
+	//MCUCR |= (1<<ISC01) | (1<<ISC00); // LA INTERRUPCION SE DETECTARA EN LOS FLANCOS POSITIVOS.
+	
 }
 
 void SET_TIMER(uint8_t diferential_time_value)
@@ -133,9 +232,9 @@ void SET_TIMER(uint8_t diferential_time_value)
 	sei();
 }
 
-void fsm()
+void FSM()
 {	
-	if (PORTB & LED_START_PAUSE) // todo
+	if (PORTB & START_PAUSE_LED) // todo
 	{
 		state = next_state;
 		switch (state)
@@ -157,9 +256,9 @@ void fsm()
 			break;
 		case STATE_SPIN:
 			diferential_time_value = SPIN(load);
-			next_state = STATE_FINISH;
+			next_state = STATE_FINAL;
 			break;
-		case STATE_FINISH:
+		case STATE_FINAL:
 			FINAL(load);
 			break;
 		default:
@@ -170,23 +269,18 @@ void fsm()
 
 uint8_t FILL(uint8_t load)
 {	
-    // Input
-	PORTA &= ~LED_STATE_WASH;
-    PORTD &= ~(LED_STATE_RINSE | LED_STATE_SPIN);
-    // Output
-    PORTB |= LED_STATE_FILL;
+	PORTA |= FILL_LED;
+	PORTD &= ~(WASH_LED | RINSE_LED );
+	PORTB &= ~SPIN_LED;
 
-    // Selección de carga baja
 	if (load == 1){
 		diferential_time_value = 1;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga media
 	else if (load == 2){
 		diferential_time_value = 2;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga pesada
 	else if (load == 3){
 		diferential_time_value = 3;
 		COUNTDOWN_TIMER(diferential_time_value);
@@ -197,24 +291,19 @@ uint8_t FILL(uint8_t load)
 
 uint8_t WASH(uint8_t load)
 {	
-    // Input
-    PORTB &= ~LED_STATE_FILL;
-	PORTD &= ~(LED_STATE_RINSE | LED_STATE_SPIN);
-    // Output
-	PORTA |= LED_STATE_WASH;
+	PORTD |= WASH_LED;
+	PORTD &= ~RINSE_LED ;
+	PORTA &= ~FILL_LED;
+	PORTB &= ~SPIN_LED;
 
-    // Selección de carga baja
 	if (load == 1){
 		diferential_time_value = 3;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga media
 	else if (load == 2){
 		diferential_time_value = 5;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga pesada
-
 	else if (load == 3){
 		diferential_time_value = 7; // TODO: No se pudo usar los dos displays al mismo tiempo
 		COUNTDOWN_TIMER(diferential_time_value);
@@ -225,24 +314,19 @@ uint8_t WASH(uint8_t load)
 
 uint8_t RINSE(uint8_t load)
 {	
-    // Input
-    PORTB &= ~LED_STATE_FILL;
-	PORTA &= ~LED_STATE_WASH;
-	PORTD &= ~LED_STATE_SPIN;
-    // Output
-    PORTD |= LED_STATE_RINSE;
+	PORTD |= RINSE_LED;
+	PORTA &= ~FILL_LED;
+	PORTD &= ~WASH_LED;
+	PORTB &= ~SPIN_LED;
 	
-    // Selección de carga baja
 	if (load == 1){
 		diferential_time_value = 2;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga media
 	else if (load == 2){
 		diferential_time_value = 4;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga pesada
 	else if (load == 3){
 		diferential_time_value = 5;
 		COUNTDOWN_TIMER(diferential_time_value);
@@ -252,44 +336,25 @@ uint8_t RINSE(uint8_t load)
 }
 
 uint8_t SPIN(uint8_t load)
-{
-    // Input	
-	PORTB &= ~LED_STATE_FILL; 
-	PORTA &= ~LED_STATE_WASH;
-    PORTD &= ~LED_STATE_RINSE;
-    // Output
-    PORTD |= LED_STATE_SPIN;
-
-    // Selección de carga baja
+{	
+	PORTB |= SPIN_LED;
+	PORTD &= ~(WASH_LED | RINSE_LED);
+	PORTA &= ~FILL_LED;
+	
 	if (load == 1){
 		diferential_time_value = 3;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga media
 	else if (load == 2){
 		diferential_time_value = 5;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
-    // Selección de carga pesada
 	else if (load == 3){
 		diferential_time_value = 6;
 		COUNTDOWN_TIMER(diferential_time_value);
 	}
 
 	return diferential_time_value;
-}
-
-void FINAL(uint8_t load)
-{	
-	// Como final se ponen todas las leds de los estados de lavado en alto 
-	PORTB |= LED_STATE_FILL;
-	PORTA |= LED_STATE_WASH;
-    PORTD |= (LED_STATE_RINSE | LED_STATE_SPIN);
-	// Reinicio de TIMER
-	diferential_time_value = 0; 
-	COUNTDOWN_TIMER(diferential_time_value);
-	SET_TIMER(diferential_time_value);
-
 }
 
 void COUNTDOWN_TIMER(int timer)
@@ -303,7 +368,15 @@ void COUNTDOWN_TIMER(int timer)
 	LED_7s_display(0, 0);
 }
 
-// Display 7 segmentos
+void FINAL(uint8_t load)
+{	
+	// Como final se ponen todas las leds de los estados de lavado en alto 
+	PORTD |= RINSE_LED;
+	PORTA |= FILL_LED;
+	PORTD |= WASH_LED;
+	PORTB |= SPIN_LED;
+}
+
 void LED_7s_display(int valor, int display)
 {
 	PORTB &= 0b10000111;
@@ -352,111 +425,3 @@ void LED_7s_display(int valor, int display)
 		else PORTB |= 0b01001100;
 	}
 }
-
-//////// INTERRUPCIONES ////////
-
-// RESET
-ISR(INT1_vect) 
-{
-	if(PIND & RESET_BUTTON){
-		state = STATE_IDE;
-		PORTB &= ~LED_STATE_FILL;
-		PORTA &= ~LED_STATE_WASH;
-		PORTD &= ~LED_STATE_RINSE;
-		PORTD &= ~LED_STATE_SPIN;
-		// Reinicio de TIMER
-		diferential_time_value = 0; 
-		COUNTDOWN_TIMER( diferential_time_value);
-		SET_TIMER(diferential_time_value);
-	}
-	else {
-		state = STATE_IDE;
-		PORTB |= ~LED_STATE_FILL;
-		PORTA |= ~LED_STATE_WASH;
-		PORTD |= ~LED_STATE_RINSE;
-		PORTD |= ~LED_STATE_SPIN;
-		// Reinicio de TIMER
-		diferential_time_value = 0; 
-		COUNTDOWN_TIMER( diferential_time_value);
-		SET_TIMER(diferential_time_value);		
-	}
-}
-
-// START/PAUSE
-ISR(INT0_vect) 
-{
-	if(PIND & START_PAUSE_BUTTON){
-		//Cuando el timer esta activado se detiene
-		if (TIMSK & (1<<TOIE0)){
-			TIMSK &= ~(1<<TOIE0);
-			PORTB &= ~LED_START_PAUSE;
-		}
-		//Si esta detenido lo iniciamos
-		else{
-			TIMSK |= (1<<TOIE0);
-			PORTB |= LED_START_PAUSE;
-		}
-	}
-}
-
-// LOW 
-ISR(PCINT1_vect)
-{
-	if(PIND & LOW_LOAD_BUTTON){
-		load = 1;
-		PORTA |= LED_LOW_LOAD;
-	}
-	else {
-		PORTA &= ~LED_LOW_LOAD;
-	}
-
-}
-
-// MEDIUM
-ISR(PCINT0_vect) 
-{
-	if(PIND & MEDIUM_LOAD_BUTTON){
-		load = 2;
-		PORTB |= LED_MEDIUM_LOAD;
-	}
-	else {
-		PORTB &= ~LED_MEDIUM_LOAD;
-	}
-
-}
-
-//HIGH
-ISR(PCINT2_vect) 
-{
-	if(PINA & HIGH_LOAD_BUTTON){
-		load = 3;
-		PORTD |= LED_HIGH_LOAD;
-	}
-	else {
-		PORTD &= ~LED_HIGH_LOAD;
-	}
-}
-
-ISR(TIMER0_COMPA_vect) {
-  // Aqui va la rutina de interrupcion
-  // Hacer LED display cada vez que se produzca la interrupción
-	static uint8_t contador = 0;
-	contador++;
-	 
-	// Se configuró el timer0 con un prescales de 64 y reloj de 1 MHz
-	// Se ocupan 15625 ciclos del timer para que pase un segundo
-	if(contador == 15625){
-		time++;
-		time_left--;
-		//led_display();
-	}
-	else if(time == diferential_time_value){
-		//led_display(0,0);
-		TCNT0 = 0;
-		TIMSK &= ~(1<<TOIE0);
-	}
-	contador = 0;
-	time = 0;
-	time_left = 0;
-}
-
