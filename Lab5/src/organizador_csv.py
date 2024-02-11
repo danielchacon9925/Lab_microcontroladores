e2350428578b247fa5a0e144d55e69df2bde354d
sanchoerick <84211587+sanchoerick@users.noreply.github.com>
2024-02-11 12:44:33 -0600

    csv ordenados

--- Lab5/src/organizador_csv.py
+++ Lab5/src/organizador_csv.py
@@ -12,20 +12,22 @@ with open('datos_actualizados.csv', 'w') as file:
         file.write(line + '\n')
 """
 
+import re
+
 # Lista de nombres de archivos de entrada y salida
 input_files = ['punch.csv', 'down.csv', 'up.csv']
 output_files = ['punch.csv', 'down.csv', 'up.csv']
 
 # Iterar sobre cada par de archivos de entrada y salida
 for input_file, output_file in zip(input_files, output_files):
-    # Abrir el archivo original
+    # Abrir el archivo original y leer las líneas
     with open(input_file, 'r') as file:
         lines = file.readlines()
 
     # Eliminar espacios adicionales alrededor de las comas y guardar las líneas actualizadas
-    updated_lines = [line.strip().replace(' ,', ', ') for line in lines]
+    updated_lines = [re.sub(r'\s*,\s*', ',', line) for line in lines]
 
     # Escribir las líneas actualizadas en un nuevo archivo CSV
     with open(output_file, 'w') as file:
         for line in updated_lines:
-            file.write(line + '\n')
+            file.write(line)
