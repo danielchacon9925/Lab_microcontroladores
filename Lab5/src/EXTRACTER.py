6364673e7c3cb32230abd565891e25eafa1b2363
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2024-02-11 08:21:28 -0600

    Extracter funcionando
    Se extraen datos de punch.csv

---
+++ Lab5/src/EXTRACTER.py
@@ -0,0 +1,38 @@
+import serial
+from datetime import datetime
+import csv
+
+# Open a csv file and set it up to receive comma-delimited input
+logging = open('punch.csv', mode='a', newline='')
+writer = csv.writer(logging, delimiter=",", escapechar=' ', quoting=csv.QUOTE_NONE)
+
+# Open a serial port that is connected to an Arduino
+ser = serial.Serial('/dev/ttyACM0')
+ser.flushInput()
+
+# Manually write the CSV header without the timestamp
+writer.writerow(["Data"])
+
+# Write out a single character encoded in utf-8
+ser.write(bytes('x', 'utf-8'))
+
+while True:
+    # Read in data from Serial until \n (new line) received
+    ser_bytes = ser.readline()
+
+    # Convert received bytes to text format
+    decoded_bytes = ser_bytes.strip().decode("utf-8")  # Strip whitespace from both ends
+
+    # If Arduino has sent a string "stop", exit loop
+    if decoded_bytes == "stop":
+        break
+
+    # Check if decoded_bytes is not empty before writing to CSV
+    if decoded_bytes:
+        # Write received data to CSV file
+        writer.writerow([decoded_bytes])
+
+# Close port and CSV file to exit
+ser.close()
+logging.close()
+print("logging finished")
